50978e2c1cc47c73e8e9e333786d3f9e246c519d
Akarshgu <akarshgupta705415@gmail.com>
2023-03-13 00:09:01 +0530

    ok

--- src/service/api.js
+++ src/service/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-
+// const URL = 'http://localhost:8800';
 
 
 export const getNews = async () => {
     try {
-        const a=await axios.get('http://localhost:8800/news?page=0&size=25')
+        const a=await axios.get('https://inshortbackend.onrender.com/news?page=0&size=25')
         console.log(a)
         return a;
         
